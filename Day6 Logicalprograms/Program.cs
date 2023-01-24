777eaa267324e3cb8316214269b0ee459fd0b4e4
varsha Gunjal <varshashingote01@gmail.com>
2023-01-24 21:37:52 +0530

    [varsha] welcome message

---
+++ Day6 Logicalprograms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Logicalprograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical Programs");
+        }
+    }
+}
